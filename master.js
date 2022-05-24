f307ff12f1e8fb2a91a9e872ad9d2e081139b102
jungyeoulyi <105956220+jungyeoulyi@users.noreply.github.com>
2022-05-24 21:24:36 +0900

    Update master.js
    hihi3

--- master.js
+++ master.js
@@ -2,5 +2,5 @@ var branch = 'main';
 const tool = 'sourcetree';
 let pull = 'sourcetree pull';//local
 let push = 'sourcetree push';//remote
-var branch1 = 'hihi';//hihi2
+var branch1 = 'hihi';//hihi3
 var devBranch;
